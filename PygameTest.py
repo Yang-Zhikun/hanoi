de4ddd2f0f1dbbde20eb52c3cfd883a19913d3b8
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2026-02-26 23:25:26 +0000

    creat a new file: PygameTest.py But it not working in codespace

---
+++ PygameTest.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()    
