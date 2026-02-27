170141f5731004d2aa65d2714ea2db1f34841bd2
404 not found <776560548@qq.com>
2026-02-27 20:39:37 +0800

    Creat a new file: .gitignore, 忽略.venv虚拟环境和.vscode的配置。 Modified: pygameTest.py，成功运行并修改窗口颜色。

--- pygameTest.py
+++ pygameTest.py
@@ -1,6 +1,8 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+screen.fill("purple")          # 这里指定颜色
+pygame.display.flip()          # 或者 pygame.display.update()
 running = True
 while running:
     for event in pygame.event.get():
